2399e7a347af35801acbac1c2a1f19256979be5f
imronshoimov <imronshoimov125@gmail.com>
2023-06-14 23:18:19 +0500

    added create and copy in fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { writeFile, existsSync, readdirSync, mkdir, copyFile } from "node:fs"
+import path from "node:path"
 
-await copy();
+const filePath = path.join(process.cwd(), "src", "fs")
+const originalFilesFolder = path.join(filePath, "files")
+const copiedFilesFolder = path.join(filePath, "files_copy")
+
+const copy = async (originalFolder, copiedFolder) => {
+  // checking files_copy or files folder
+  if (existsSync(copiedFolder) || !existsSync(originalFolder)) {
+    throw Error("FS operation failed")
+  }
+
+  // creating files_copy folder
+  mkdir(copiedFolder, { recursive: true }, (err) => {
+    if (err) throw err
+  })
+
+  // reading files folder and copying all its content to files_copy folder
+  const files = readdirSync(originalFolder)
+
+  files.forEach((file) => {
+    file = "/" + file
+
+    copyFile(originalFolder + file, copiedFolder + file, (err) => {
+      if (err) throw err
+    })
+  })
+
+  console.log("Copied successfully!")
+}
+
+await copy(originalFilesFolder, copiedFilesFolder)
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile, existsSync } from "node:fs"
+import path from "node:path"
 
-await create();
+const filePath = path.join(process.cwd(), "src", "fs", "files")
+const content = "I am fresh and young"
+
+const create = async (filePath, content) => {
+  writeFile(filePath + "/fresh.txt", content, (err) => {
+    if (err) throw err
+
+    if (existsSync(filePath + "/fresh.txt")) {
+      throw Error("FS operation failed")
+    }
+
+    console.log("The file has been saved!")
+  })
+}
+
+await create(filePath, content)
