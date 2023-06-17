f368035cdfa68d7c902bde58849f86cea579bae8
imronshoimov <imronshoimov125@gmail.com>
2023-06-18 00:32:01 +0500

    finished hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHmac } from "node:crypto"
+import { readFile } from "node:fs"
+import path from "node:path"
 
-await calculateHash();
+const secret = "something"
+const filePath = path.join(
+  process.cwd(),
+  "src",
+  "hash",
+  "files",
+  "fileToCalculateHashFor.txt"
+)
+
+const calculateHash = async (secret, path) => {
+  readFile(path, "utf-8", (err, data) => {
+    if (err) throw err
+
+    const hash = createHmac("sha256", secret).update(data).digest("hex")
+
+    console.log(hash)
+  })
+}
+
+await calculateHash(secret, filePath)
