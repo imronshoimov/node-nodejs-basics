555b0fd9b5a5794f82dbd8228fd3e2f0ff028f9a
imronshoimov <imronshoimov125@gmail.com>
2023-06-18 15:34:20 +0500

    done read and write of streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
 
-await read();
+const filePath = path.join(
+  process.cwd(),
+  "src",
+  "streams",
+  "files",
+  "fileToRead.txt"
+)
+
+const read = async (path) => {
+  const readableStream = fs.createReadStream(path, "utf-8")
+
+  readableStream.on("data", (chunk) => {
+    process.stdout.write(chunk)
+  })
+
+  readableStream.on("end", () => {})
+
+  readableStream.on("error", (error) => {
+    throw Error("There is an error: ", error)
+  })
+}
+
+await read(filePath)
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
-const write = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
 
-await write();
+const filePath = path.join(
+  process.cwd(),
+  "src",
+  "streams",
+  "files",
+  "fileToWrite.txt"
+)
+
+const write = async (path) => {
+  const writableStream = fs.createWriteStream(path, "utf-8")
+
+  process.stdin.pipe(writableStream)
+  console.log("Type to write: ")
+
+  writableStream.on("finish", () => {})
+
+  writableStream.on("error", (error) => {
+    throw Error("There is an error: ", error)
+  })
+}
+
+await write(filePath)
