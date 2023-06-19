130c0a4b3c538fb2b10104fe44f16bd5f05299ca
imronshoimov <imronshoimov125@gmail.com>
2023-06-19 23:14:18 +0500

    completed worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker, isMainThread } from "worker_threads"
+import path from "path"
+import os from "os"
 
-await performCalculations();
+const workerFile = path.join(process.cwd(), "src", "wt", "worker.js")
+
+const performCalculations = async (workerFile) => {
+  const numCores = os.cpus().length
+  const results = []
+
+  function handleWorkerMessage(index, result) {
+    results[index] = result
+
+    if (results.length === numCores) {
+      console.log(results)
+    }
+  }
+
+  if (isMainThread) {
+    for (let i = 0; i < numCores; i++) {
+      const worker = new Worker(workerFile, { workerData: i + 10 })
+
+      worker.on("message", (result) => {
+        handleWorkerMessage(i, result)
+      })
+
+      worker.on("error", () => {
+        handleWorkerMessage(i, { status: "error", data: null })
+      })
+    }
+  }
+}
+
+await performCalculations(workerFile)
