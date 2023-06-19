8a53828e6676a7b0ef15acbddda4a5b60eae358f
imronshoimov <imronshoimov125@gmail.com>
2023-06-19 22:00:08 +0500

    finished zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from "node:fs"
+import { createGzip } from "zlib"
+import path from "node:path"
 
-await compress();
+const folderPath = path.join(process.cwd(), "src", "zip", "files")
+const inputFile = path.join(folderPath, "fileToCompress.txt")
+const outputFile = path.join(folderPath, "archive.gz")
+
+const compress = async (inputFile, outputFile) => {
+  const handleStream = createReadStream(inputFile)
+
+  handleStream
+    .pipe(createGzip())
+    .pipe(createWriteStream(outputFile))
+    .on("finish", () => {
+      console.log("Compressing finished!")
+    })
+}
+
+await compress(inputFile, outputFile)
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from "node:fs"
+import { createUnzip } from "zlib"
+import path from "node:path"
 
-await decompress();
+const folderPath = path.join(process.cwd(), "src", "zip", "files")
+const inputFile = path.join(folderPath, "archive.gz")
+const outputFile = path.join(folderPath, "fileToCompress.txt")
+
+const decompress = async (inputFile, outputFile) => {
+  const handleStream = createReadStream(inputFile)
+
+  handleStream
+    .pipe(createUnzip())
+    .pipe(createWriteStream(outputFile))
+    .on("finish", () => {
+      console.log("Decompressing finished!")
+    })
+}
+
+await decompress(inputFile, outputFile)
